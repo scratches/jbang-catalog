66b5e852232e73b07130ad2c8d4fcc178160c5fc
Dave Syer <david.syer@broadcom.com>
2025-06-20 08:02:50 +0100

    Allow ScriptRun.run(args) directly

--- example/ScriptRun.java
+++ example/ScriptRun.java
@@ -4,6 +4,7 @@
 //SOURCES GenericApplication.java
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.util.ClassUtils;
 
 import java.util.LinkedHashSet;
@@ -13,15 +14,23 @@ import java.util.Set;
 public class ScriptRun {
 
 	public static SpringApplicationBuilder from(Class<?> ...classes) {
+		return new SpringApplicationBuilder(extract(classes, 1));
+	}
+
+	public static ConfigurableApplicationContext run(String ...args) {
+		return from(extract(new Class[0], 2)).run(args);
+	}
+
+	private static Class<?>[] extract(Class<?>[] classes, int stackLevel) {
 		Set<Class<?>> classList = new LinkedHashSet<>(List.of(classes));
 		classList.add(example.GenericApplication.class);
-		Class<?> caller = ClassUtils.resolveClassName(new RuntimeException().getStackTrace()[1].getClassName(), null);
+		Class<?> caller = ClassUtils.resolveClassName(new RuntimeException().getStackTrace()[stackLevel].getClassName(), null);
 		if (caller != null) {
 			for (Class<?> c : caller.getDeclaredClasses()) {
 				classList.add(c);
 			}
 		}
-		return new SpringApplicationBuilder(classList.toArray(new Class[0]));
+		return classList.toArray(new Class[0]);
 	}
 
 }
