597c4de93e0bb193d20c568a8d3370c518c7d7ab
Dave Syer <david.syer@broadcom.com>
2025-06-20 13:16:19 +0100

    Add caller to config sources

--- example/SpringScript.java
+++ example/SpringScript.java
@@ -3,34 +3,35 @@
 //DEPS org.springframework.boot:spring-boot-starter
 //SOURCES GenericApplication.java
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.util.ClassUtils;
-
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.ClassUtils;
+
 public class SpringScript {
 
-	public static SpringApplicationBuilder from(Class<?> ...classes) {
-		return new SpringApplicationBuilder(extract(classes, 1));
+	public static SpringApplicationBuilder from(Class<?>... classes) {
+		return new SpringApplicationBuilder(extract(classes, 2));
 	}
 
-	public static ConfigurableApplicationContext run(String ...args) {
-		return from(extract(new Class[0], 2)).run(args);
+	public static ConfigurableApplicationContext run(String... args) {
+		return new SpringApplicationBuilder(extract(new Class[0], 2)).run(args);
 	}
 
 	private static Class<?>[] extract(Class<?>[] classes, int stackLevel) {
 		Set<Class<?>> classList = new LinkedHashSet<>(List.of(classes));
 		classList.add(example.GenericApplication.class);
-		Class<?> caller = ClassUtils.resolveClassName(new RuntimeException().getStackTrace()[stackLevel].getClassName(), null);
+		Class<?> caller = ClassUtils.resolveClassName(new RuntimeException().getStackTrace()[stackLevel].getClassName(),
+				null);
 		if (caller != null) {
 			for (Class<?> c : caller.getDeclaredClasses()) {
 				classList.add(c);
 			}
+			classList.add(caller);
 		}
 		return classList.toArray(new Class[0]);
 	}
-
 }
