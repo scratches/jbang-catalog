6d7676fd72630ede4a7aa2ae15f4dfd0167b875b
Dave Syer <david.syer@broadcom.com>
2025-06-24 14:12:02 +0100

    Add springbom alias

---
+++ example/SpringBom.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS org.springframework.boot:spring-boot-dependencies:3.5.0@pom
+//DEPS org.springframework.cloud:spring-cloud-dependencies:2025.0.0@pom
+//DEPS org.springframework.grpc:spring-grpc-dependencies:0.8.0@pom
+//DEPS org.springframework.ai:spring-ai-bom:1.0.0@pom
+//DEPS org.springframework.modulith:spring-modulith-bom:1.4.1@pom
+//DEPS org.springframework.boot:spring-boot-starter
+
+final class SpringBom {
+}
