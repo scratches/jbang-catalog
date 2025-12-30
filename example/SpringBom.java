25f4bc56702bc77f00b04698c7a1b562025abc3d
Dave Syer <david.syer@broadcom.com>
2025-12-30 08:13:25 +0000

    Update versions

--- example/SpringBom.java
+++ example/SpringBom.java
@@ -1,9 +1,8 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS org.springframework.boot:spring-boot-dependencies:4.0.1@pom
-//DEPS org.springframework.cloud:spring-cloud-dependencies:2025.0.0@pom
-//DEPS org.springframework.grpc:spring-grpc-dependencies:0.11.0@pom
-//DEPS org.springframework.ai:spring-ai-bom:1.0.2@pom
-//DEPS org.springframework.modulith:spring-modulith-bom:1.4.1@pom
+//DEPS org.springframework.cloud:spring-cloud-dependencies:2025.1.0@pom
+//DEPS org.springframework.grpc:spring-grpc-dependencies:1.0.0@pom
+//DEPS org.springframework.modulith:spring-modulith-bom:2.0.1@pom
 //DEPS org.springframework.boot:spring-boot-starter
 
 final class SpringBom {
