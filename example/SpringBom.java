8adfdb04c8267c1cd0723239f104f931b4905677
Dave Syer <david.syer@broadcom.com>
2025-12-30 08:09:14 +0000

    Update to Boot 4.0.1

--- example/SpringBom.java
+++ example/SpringBom.java
@@ -1,5 +1,5 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS org.springframework.boot:spring-boot-dependencies:3.5.5@pom
+//DEPS org.springframework.boot:spring-boot-dependencies:4.0.1@pom
 //DEPS org.springframework.cloud:spring-cloud-dependencies:2025.0.0@pom
 //DEPS org.springframework.grpc:spring-grpc-dependencies:0.11.0@pom
 //DEPS org.springframework.ai:spring-ai-bom:1.0.2@pom
